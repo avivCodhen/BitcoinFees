dc75db2b0d7d8298ebbfcd198892a79685ad7bc8
Aviv Cohen <kaazz931@gmail.com>
2019-09-05 23:46:23 +0300

    currencies display

--- Fees/Controllers/HomeController.cs
+++ Fees/Controllers/HomeController.cs
@@ -1,25 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Fees.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Fees.Controllers
 {
     public class HomeController : Controller
     {
+        private const int SatoshiDivideToBtc = 100000000;
+
         public async Task<IActionResult> Index()
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync("https://bitcoinfees.earn.com/api/v1/fees/recommended");
-            var bitcoinFee = await response.Content.ReadAsAsync<BitCoinFee>();
-            return View(bitcoinFee);
-        }
+            var blocks = new[] {2, 6, 48};
+            var list = new List<BlockFee>();
+            foreach (var block in blocks)
+            {
+                var response = await httpClient.GetAsync($"https://estimatefee.com/n/{block}");
+                var results = await response.Content.ReadAsStringAsync();
+                var satoshiPerByte = float.Parse(results, CultureInfo.InvariantCulture.NumberFormat);
+                var btc = (satoshiPerByte* 100000 *226)/100000000;
+                var btcResponse = await httpClient.GetAsync("https://blockchain.info/ticker");
+                var btcResults = await btcResponse.Content.ReadAsAsync<BitCoinFee>();
+                var nisResponse = await httpClient.GetAsync("https://api.exchangeratesapi.io/latest");
+                var nisResults = await nisResponse.Content.ReadAsAsync<RootObject>();
 
+                list.Add(new BlockFee(){NumBlock = block,
+                    SatoshiPerByte = satoshiPerByte,
+                    Btc = btc.ToString("F6").TrimEnd('0'),
+                    Dollar = (btcResults.USD.last*btc).ToString("F6").TrimEnd('0'),
+                    Nis = (nisResults.rates.ILS * btcResults.USD.last * btc).ToString("F6").TrimEnd('0')
+                });
+            }
+            return View(list);
+        }
         public IActionResult Privacy()
         {
             return View();
@@ -28,7 +49,7 @@ namespace Fees.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
     }
 }
