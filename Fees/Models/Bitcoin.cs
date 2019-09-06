7cfe467fe2d05f6e3e5903eaaf50913715617e54
Aviv Cohen <kaazz931@gmail.com>
2019-09-06 10:46:11 +0300

    removed fade animation

---
+++ Fees/Models/Bitcoin.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Fees.Models
+{
+    public class USD
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class AUD
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class BRL
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class CAD
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class CHF
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class CLP
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class CNY
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class DKK
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class EUR
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class GBP
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class HKD
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class INR
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class ISK
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class JPY
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class KRW
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class NZD
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class PLN
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class RUB
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class SEK
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class SGD
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class THB
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class TWD
+    {
+        public double __invalid_name__15m { get; set; }
+        public double last { get; set; }
+        public double buy { get; set; }
+        public double sell { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class BitCoinFee
+    {
+        public USD USD { get; set; }
+        public AUD AUD { get; set; }
+        public BRL BRL { get; set; }
+        public CAD CAD { get; set; }
+        public CHF CHF { get; set; }
+        public CLP CLP { get; set; }
+        public CNY CNY { get; set; }
+        public DKK DKK { get; set; }
+        public EUR EUR { get; set; }
+        public GBP GBP { get; set; }
+        public HKD HKD { get; set; }
+        public INR INR { get; set; }
+        public ISK ISK { get; set; }
+        public JPY JPY { get; set; }
+        public KRW KRW { get; set; }
+        public NZD NZD { get; set; }
+        public PLN PLN { get; set; }
+        public RUB RUB { get; set; }
+        public SEK SEK { get; set; }
+        public SGD SGD { get; set; }
+        public THB THB { get; set; }
+        public TWD TWD { get; set; }
+    }
+}
---
+++ Fees/Models/BlockFee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fees.Models
+{
+    public class BlockFee
+    {
+        public int NumBlock { get; set; }
+        public float SatoshiPerByte { get; set; }
+        public string Btc { get; set; }
+        public string BtcSegwit { get; set; }
+        public string Dollar { get; set; }
+        public string DollarSegwit { get; set; }
+        public string Nis { get; set; }
+        public string NisSegwit { get; set; }
+
+    }
+}
